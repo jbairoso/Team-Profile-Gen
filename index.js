8038c1fae4632e843d70e72dc9f4e185acb831fe
jbairoso <jbairoso@gmail.com>
2022-05-27 18:26:29 -0700

    team list

--- index.js
+++ index.js
@@ -2,17 +2,25 @@ const inquirer = require("inquirer");
 const { writeFile } = require("./src/write-file.js");
 const generatePage = require("./src/page-template");
 const fs = require("fs");
-const Employee =require('./lib/Employee');
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
-const Manager = require('./lib/Manager');
+const Employee = require("./lib/Employee");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
 
-function promptUser() {
+const team = [];
+
+const promptUser = () => {
   return inquirer.prompt([
     {
+      type: "list",
+      name: "role",
+      message: "Please select the employee's role (Required)",
+      choices: ["Manager", "Engineer", "Intern"],
+    },
+    {
       type: "input",
-      name: "name",
-      message: "What is your name? (Required)",
+      name: "id",
+      message: "Please enter your employee ID",
       validate: (nameInput) => {
         if (nameInput) {
           return true;
@@ -20,12 +28,7 @@ function promptUser() {
           console.log("Please enter your Name!");
           return false;
         }
-      }
-    },
-    {
-      type: "input",
-      name: "id",
-      message: "Please enter your employee ID",
+      },
     },
     {
       type: "input",
@@ -35,4 +38,6 @@ function promptUser() {
   ]);
 };
 
-promptUser();
+promptUser().then((team) => {
+  return generatePage(team);
+});
